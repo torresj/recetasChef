931ba221f46cc924d3a043cca032838ec597fbed
Renato L. Ramírez <renato@osl-tac-2.(none)>
2014-02-05 11:02:56 +0100

    añadidas recetas de chef

---
+++ cookbooks/lxde/recipes/default.rb
@@ -0,0 +1,3 @@
+#Recipe for installing emacs and creating a directory and a file
+package 'task-lxde-desktop'
+
---
+++ solo.rb
@@ -0,0 +1,3 @@
+#Configuration file for chef-solo
+cookbook_path File.expand_path("../cookbooks", __FILE__)
+json_attribs File.expand_path("../node.json", __FILE__)
